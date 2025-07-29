aaca8155ec1d50a0079e4d7a18a39c953b3c0fe7
rajvee123 <152398892+rajvee123@users.noreply.github.com>
2025-07-29 20:41:06 +0530

    Update argv.c

--- week1/argv.c
+++ week1/argv.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[]){
-    printf("argc: %d\n", argc);
-    printf("argv:\n");
-    for(int i = 0; i < argc; i++) {
-        printf("%s\n", argv[i]);
+int main(int count, char *args[]) {
+    printf("Total arguments passed: %d\n", count);
+
+    if (count > 0) {
+        puts("Arguments received:");
+        for (int index = 0; index < count; ++index) {
+            printf("  [%d] %s\n", index, args[index]);
+        }
+    } else {
+        puts("No arguments provided.");
     }
+
     return 0;
 }
+
